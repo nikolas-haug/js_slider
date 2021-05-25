116236117e87aaae6919222833e05c544d8a994a
Nikolas Haug <nikohaug21@gmail.com>
2021-05-25 13:06:25 -0500

    clean up code

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-// const width = '33.3333%';
-// const width = '25%';
 const mainContainer = document.querySelector('.main-container');
 const sliderContainer = document.querySelector('.slider-container');
 const slides = document.querySelectorAll('.slide');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
-// from the tutorial TODO: refactor this
 const show = 3;
+// Currently getting the width from the main container
 const width = mainContainer.offsetWidth / show;
 
-// $('.slide').css({ flexBasis: width, minWidth: width });
-// $('.slider-container').width(slideWidth * sliderLength);
-
-
 function init() {
+    setUpSlides();
+    addActiveClass();
+}
+
+function setUpSlides() {
     slides.forEach((slide, i) => {
-        slide.style.flexBasis = `${width}px`;
-        slide.style.minWidth = `${width}px`;
+        // set slide widths dynamically or use CSS with media queries
+        // slide.style.flexBasis = `${width}px`;
+        // slide.style.minWidth = `${width}px`;
     
         slide.setAttribute('data-slide-index', i + 1);
-    
         const dot = document.createElement('span');
         dot.setAttribute('class', 'dot');
         dot.setAttribute('data-slide-dot', i + 1);
         dot.addEventListener('click', moveToDot);
-    
         document.querySelector('.slide-navigation').appendChild(dot);
     });
-    
-    addActiveClass();
 }
 
 function sliderLeft() {
     let firstSlide = document.querySelector('.slide:first-child');
-    firstSlide.style.marginLeft = `-${width}px`;
+    // set the viewport size here according to media query
+    if(window.innerWidth < 650) {
+        firstSlide.style.marginLeft = `-100%`;
+    } else {
+        firstSlide.style.marginLeft = `-${width}px`;
+    }
     setTimeout(() => {
         sliderContainer.append(firstSlide);
         firstSlide.style.marginLeft = 0;
@@ -46,13 +47,16 @@ function sliderLeft() {
 function sliderRight() {
     let firstSlide = document.querySelector('.slide:first-child');
     let lastSlide = document.querySelector('.slide:last-child');
-
-    lastSlide.style.marginLeft = `-${width}px`;
-
-    sliderContainer.prepend(lastSlide);
-
-    firstSlide.style.marginLeft = `${width}`;
-
+    // set the viewport size here according to media query
+    if(window.innerWidth < 650) {
+        lastSlide.style.marginLeft = `-100%`;
+        sliderContainer.prepend(lastSlide);
+        firstSlide.style.marginLeft = `$100%`;
+    } else {
+        lastSlide.style.marginLeft = `-${width}px`;
+        sliderContainer.prepend(lastSlide);
+        firstSlide.style.marginLeft = `${width}`;
+    }
     firstSlide.style.marginLeft = 0;
     setTimeout(() => {
         lastSlide.style.marginLeft = 0;
@@ -76,15 +80,10 @@ function addActiveClass() {
 
 function moveToDot() {
     let dotDataIndex = this.getAttribute('data-slide-dot');
-    // let selectedSlide = document.querySelector(`.slide[data-slide-index="${dotDataIndex}"]`);
-
     let currentFirstSlide = document.querySelector('.slider-container div:first-child');
     let slideIndex = currentFirstSlide.getAttribute('data-slide-index');
 
-    console.log(dotDataIndex - slideIndex);
-
     let increment = dotDataIndex - slideIndex;
-    console.log(increment);
 
     if(increment < 0) {
         for(let i = 0; i < Math.abs(increment); i++) {
@@ -97,8 +96,6 @@ function moveToDot() {
             sliderContainer.append(firstSlide);
         }
     }
-
-
     removeActiveClass();
     addActiveClass();
 }
